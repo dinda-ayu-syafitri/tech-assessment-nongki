63a20ce05e30f49a3654f382f9f9889257811882
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2024-12-03 20:25:32 +0700

    Initial Commit

---
+++ nongkiWeather/nongkiWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  nongkiWeatherApp.swift
+//  nongkiWeather
+//
+//  Created by Dinda Ayu Syafitri on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct nongkiWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
